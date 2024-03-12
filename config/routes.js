ca70d7d09fe8b26bb5ad0b32af4032fbe1b74e75
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2024-03-12 11:48:56 -0300

    [FEAT] evaluation model and routes

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,10 @@ module.exports.routes = {
   //Users
   'post /login': 'UsersController.login',
   'post /generateToken': 'UsersController.generateToken',
-  'post /resetPassword': 'UsersController.resetPassword'
+  'post /resetPassword': 'UsersController.resetPassword',
+  //Evaluations
+  'get /findByGame/:game_id': 'EvaluationsController.getByGame',
+  'get /findByUser/:user_id': 'EvaluationsController.getByUser'
+
   
 };
