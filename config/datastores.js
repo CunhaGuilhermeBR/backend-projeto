1ab96e3b4936980839db3acbf74cf36619f61f1f
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2024-03-12 11:10:24 -0300

    [REFACTOR] remove commented url in datastore

--- config/datastores.js
+++ config/datastores.js
@@ -4,10 +4,7 @@ dotenv.config();
 module.exports.datastores = {
   default: {
     adapter: 'sails-mongo',
-    //url: 'mongodb://admin:E9aetuoxCfsGwpav@cluster0.fqs5xdf.mongodb.net:27017/?retryWrites=true&w=majority'
     url: process.env.MONGO_URL
-
   }
-
 
 };
